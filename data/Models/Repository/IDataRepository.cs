1edbf84038451fc2c1d617858ddfe91e36b7d35f
Maxime Brossard <maxime.brossard5@gmail.com>
2025-01-06 10:37:53 +0100

    fix: adding access DB from data repo

---
+++ data/Models/Repository/IDataRepository.cs
@@ -0,0 +1,14 @@
+﻿using data.Models.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace data.Models.Repository
+{
+    public interface IDataRepository<TEntity>
+    {
+        Task<ActionResult<IEnumerable<TEntity>>> GetAllAsync();
+        Task<ActionResult<TEntity>> GetByIdAsync(int id);
+        Task<ActionResult<Events>> AddAsync(TEntity entity);
+        Task<ActionResult<Events>> UpdateAsync(TEntity entityToUpdate, TEntity entityUpdated);
+        Task<ActionResult> DeleteAsync(TEntity entity);
+    }
+}
