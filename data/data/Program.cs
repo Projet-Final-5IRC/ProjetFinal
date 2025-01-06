e46d8ec4e53ce83d5fd703fee310e752833161f5
Maxime Brossard <maxime.brossard5@gmail.com>
2025-01-06 13:15:32 +0100

    feat: adding genre crud

--- data/data/Program.cs
+++ data/data/Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddScoped<IDataRepository<Events>, EventsManager>();
+builder.Services.AddScoped<IDataRepository<Genres>, GenresManager>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
