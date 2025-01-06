e8108fc5135cdc4c29ff9eb3f144b395360d81f5
RemiLeCodeur <rem.louedec@laposte.net>
2025-01-06 09:52:00 +0100

    ajout app mobile

---
+++ front-mobile/cinefouine/lib/modules/bottomNavigation/view.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'view.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$tabBarIndexNotifierHash() =>
+    r'e429ce253979f91040dbb6a650cf78cea67c2cf9';
+
+/// See also [_TabBarIndexNotifier].
+@ProviderFor(_TabBarIndexNotifier)
+final _tabBarIndexNotifierProvider =
+    AutoDisposeNotifierProvider<_TabBarIndexNotifier, int>.internal(
+  _TabBarIndexNotifier.new,
+  name: r'_tabBarIndexNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$tabBarIndexNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$TabBarIndexNotifier = AutoDisposeNotifier<int>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
