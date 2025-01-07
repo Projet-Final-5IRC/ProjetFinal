0e4ceaece339baae47b3f70aa3dd735e44e7f76c
Maxime <maxime.brossard5@gmail.com>
2025-01-07 21:53:58 +0100

    feat: finishing logic in event ms

--- ms-evt/ms-evt/Services/DataService.cs
+++ ms-evt/ms-evt/Services/DataService.cs
@@ -1,5 +1,9 @@
-﻿using ms_evt.Models.DTO;
+﻿using Microsoft.AspNetCore.Mvc;
+using ms_evt.Models.DTO;
 using Newtonsoft.Json;
+using System;
+using System.Net;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace ms_evt.Services
 {
@@ -17,17 +21,37 @@ namespace ms_evt.Services
             _httpClient.BaseAddress = new Uri(apiBaseUrl);
         }
 
-        public async Task<List<EventDTO>> GetEventAsync<EventDTO>(string endpoint)
+        public async Task<List<EventDTO>> GetEventsAsync(string endpoint)
         {
             var response = await _httpClient.GetStringAsync(endpoint);
             return JsonConvert.DeserializeObject<List<EventDTO>>(response);
         }
 
-        public async Task<bool> PostDataAsync<EventDTO>(string endpoint, EventDTO data)
+        public async Task<EventDTO> GetEventByIdAsync(string endpoint,int id)
+        {
+            var response = await _httpClient.GetStringAsync($"{endpoint}/{id}");
+            return JsonConvert.DeserializeObject<EventDTO>(response);
+        }
+
+        public async Task<HttpStatusCode> PostEventAsync(string endpoint, EventDTO data)
         {
             var content = new StringContent(JsonConvert.SerializeObject(data), System.Text.Encoding.UTF8, "application/json");
             var response = await _httpClient.PostAsync(endpoint, content);
-            return response.IsSuccessStatusCode;
+
+            return response.StatusCode;
+        }
+
+        public async Task<HttpStatusCode> DeleteEventAsync(string endpoint, int id)
+        {
+            var response = await _httpClient.DeleteAsync($"{endpoint}/{id}");
+            return response.StatusCode;
+        }
+
+        public async Task<HttpStatusCode> PutEventAsync(string endpoint, int id,EventDTO eventDTO)
+        {
+            var content = new StringContent(JsonConvert.SerializeObject(eventDTO), System.Text.Encoding.UTF8, "application/json");
+            var response = await _httpClient.PutAsync($"{endpoint}/{id}",content);
+            return response.StatusCode;
         }
     }
 }
