a5250cc3056ab2ea8c914fc75c41a7495f52a976
RemiLeCodeur <rem.louedec@laposte.net>
2025-01-08 14:19:15 +0100

    creation client dio

--- front-mobile/cinefouine/lib/data/entities/repositories/event_repository.g.dart
+++ front-mobile/cinefouine/lib/data/entities/repositories/event_repository.g.dart
@@ -1,26 +1,27 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'mainAppBar.dart';
+part of 'event_repository.dart';
 
 // **************************************************************************
 // RiverpodGenerator
 // **************************************************************************
 
-String _$isSearchModeActifHash() => r'1f8b9818573450e8698ab65f091fb7c0b9b566c6';
+String _$eventRepositoryHash() => r'86b3f1fb345ed8b6b579e4ea2a464cff2153d1ee';
 
-/// See also [IsSearchModeActif].
-@ProviderFor(IsSearchModeActif)
-final isSearchModeActifProvider =
-    AutoDisposeNotifierProvider<IsSearchModeActif, bool>.internal(
-  IsSearchModeActif.new,
-  name: r'isSearchModeActifProvider',
+/// See also [eventRepository].
+@ProviderFor(eventRepository)
+final eventRepositoryProvider = Provider<EventRepository>.internal(
+  eventRepository,
+  name: r'eventRepositoryProvider',
   debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
       ? null
-      : _$isSearchModeActifHash,
+      : _$eventRepositoryHash,
   dependencies: null,
   allTransitiveDependencies: null,
 );
 
-typedef _$IsSearchModeActif = AutoDisposeNotifier<bool>;
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef EventRepositoryRef = ProviderRef<EventRepository>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
