636094de1d0683e50b6aef7a30605a5674b4fce1
Maxime Brossard <maxime.brossard5@gmail.com>
2025-01-09 17:17:07 +0100

    fix: data genre bad use

--- data/data/Migrations/20250109161112_EventEditGenre.Designer.cs
+++ data/data/Migrations/20250109161112_EventEditGenre.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using data.Models.EntityFramework;
@@ -11,9 +12,11 @@ using data.Models.EntityFramework;
 namespace data.Migrations
 {
     [DbContext(typeof(EventDBContext))]
-    partial class EventDBContextModelSnapshot : ModelSnapshot
+    [Migration("20250109161112_EventEditGenre")]
+    partial class EventEditGenre
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- data/data/Models/DTO/GenreDTO.cs
+++ data/data/Models/DTO/GenreDTO.cs
@@ -6,12 +6,14 @@ namespace data.Models.DTO
     public class GenreDTO
     {
         public int IdGenre { get; set; }
+        public int IdTMDBGenre { get; set; }
         public string GenreName { get; set; }
         public List<int> EventsId { get; set; }
 
         public GenreDTO(Genres genre)
         {
             this.IdGenre = genre.IdGenre;
+            this.IdTMDBGenre = genre.IdTMDBGenre;
             this.GenreName = genre.GenreName;
             this.EventsId = genre.EventsGenre
                                     .Select(events => events.IdEvent)
