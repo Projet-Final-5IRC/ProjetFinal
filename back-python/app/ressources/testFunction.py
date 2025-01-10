1045879859dc6cdd57bfdc3940157b7de43bb878
GJBoX <julien.buc2@gmail.com>
2025-01-10 17:10:14 +0100

    api fonctionnelle

--- back-python/app/ressources/testFunction.py
+++
@@ -1,6 +0,0 @@
-def traitement_like(exitDataUserSeen, entry_json, exitData):
-    likes_str = entry_json.get("likes", "")
-    likes_list = list(map(int, likes_str.split(',')))  # Conversion en liste d'entiers
-    
-    
-    
