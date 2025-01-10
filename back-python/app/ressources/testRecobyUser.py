8ac587550d7961b1dc03f8eccfd44a2b87c1b22e
GJBoX <julien.buc2@gmail.com>
2025-01-10 10:59:04 +0100

    Système de reco 100%, mise en place de l'api

--- back-python/app/ressources/testRecobyUser.py
+++ back-python/app/ressources/testRecobyUser.py
@@ -6,13 +6,15 @@ import random
 import numpy as np
 from sklearn.preprocessing import LabelEncoder
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
 
 pathUser = "../../data/User/userTest1.json"
 pathMovie = "../../data/Movies/movies.json"
 
-exitDataUserLike = []
 exitDataUserSeen = []
 exitDataMovie = []
+userLike = []
+dataUserLike = []
 
 #### Data ####
 # User
@@ -28,58 +30,83 @@ with open(pathMovie, 'r') as file:
 ## User Training
 print("-----------------------------------")
 print("-----------Fonction traitment------")
-exitDataUser = fct.traitement_like(entry_data, entry_json, exitDataUserLike) 
-print(f"exitDataUser à la fin de la boucle: {exitDataUser}")
-print("-----------------------------------")
-print("-----------------------------------")
+# exitDataUser = fct.traitement_like(entry_data, entry_json, exitDataUserLike) 
+# print(f"exitDataUser à la fin de la boucle: {exitDataUser}")
+# print("-----------------------------------")
+# print("-----------------------------------")
 exitDataUserSeen = fct.traitement_seen(entry_data, entry_json, exitDataUserSeen)
-print(f"exitDataUser à la fin de la boucle: {exitDataUserSeen}")
-print("-----------------------------------")
-print("-----------------------------------")
+dataUserLike= exitDataUserSeen[0]
+userLike = exitDataUserSeen[1]
+# print(f"exitDataUser data à la fin de la boucle: {exitDataUserSeen[0]}")
+# print("-----------------------------------")
+# print(f"exitDataUser likes à la fin de la boucle: {exitDataUserSeen[1]}")
+# print("-----------------------------------")
 ## Movie Data for prediction (total database)
 exitDataMovie = fct.traitement_movie(entry_data, exitDataMovie)
 print("---------Fin traitement------------")
+print("-----------------------------------")
+print("------Création Dataframe-----------")
 
-###########################à faire ###########################
-# # Formater les données pour les adapter au DataFrame
-# df_user = [[d[key] for key in d.keys()] for d in exitDataUser] 
-# print("Le tableau user :")
-# print(df_user)
-# print("\n")
+# Création des DataFrames
+dataframeUserLike = pd.DataFrame(userLike, columns=["like"])
+dataframeUserLike = dataframeUserLike.astype(str)  # Tout convertir en chaînes
+print("dataframeUserLike")
+print(dataframeUserLike)
+print("-----------------------------------")
+dataframeUserLikeData = pd.DataFrame(dataUserLike)
+dataframeUserLikeData = dataframeUserLikeData.astype(str)  # Tout convertir en chaînes
+dataframeMovie = pd.DataFrame(exitDataMovie)
+dataframeMovie = dataframeMovie.astype(str)  # Tout convertir en chaînes
+print("-----------------------------------")
+print("dataframeUserLikeData")
+print(dataframeUserLikeData)
+print("-----------Fin Dataframe-----------")
+print("-----------------------------------")
+print("--------Encodage-------------------")
+# Transformer les colonnes de dataframeUserLikeData
+le1 = LabelEncoder()
+dataframeUserLikeData["adult"] = le1.fit_transform(dataframeUserLikeData["adult"])
 
-# df_data = [[d[key] for key in d.keys()] for d in exitDataMovie] 
-# print("Le tableau des data:")
-# print(df_data)
-# print("\n")
+le2 = LabelEncoder()
+dataframeUserLikeData["id"] = le2.fit_transform(dataframeUserLikeData["id"])
 
-# # Création des DataFrames
-# dataframeUser = pd.DataFrame(df_data, columns=["year", "make", "orientation", "width", "height"])
-# dataframeData = pd.DataFrame(result, columns=["favorite"])
+le3 = LabelEncoder()
+dataframeUserLikeData["revenue"] = le3.fit_transform(dataframeUserLikeData["revenue"])
 
-# # Génération des étiquettes numériques
-# le1 = LabelEncoder()
-# dataframe["year"] = le1.fit_transform(dataframe["year"])
+le4 = LabelEncoder()
+dataframeUserLikeData["runtime"] = le4.fit_transform(dataframeUserLikeData["runtime"])
 
-# le2 = LabelEncoder()
-# dataframe["make"] = le2.fit_transform(dataframe["make"])
+# Transformer les colonnes de dataframeMovie
+le5 = LabelEncoder()
+dataframeMovie["adult"] = le5.fit_transform(dataframeMovie["adult"])
 
-# le3 = LabelEncoder()
-# dataframe["orientation"] = le3.fit_transform(dataframe["orientation"])
+le6 = LabelEncoder()
+dataframeMovie["id"] = le6.fit_transform(dataframeMovie["id"])
 
-# # Utilisation des classificateurs d'arbres de décision
-# rfc = RandomForestClassifier(
-#     n_estimators=10,
-#     max_depth=3,
-#     random_state=0,
-# )
+le7 = LabelEncoder()
+dataframeMovie["revenue"] = le7.fit_transform(dataframeMovie["revenue"])
 
-# # Entraînement du classificateur RFC sur les données
-# rfc = rfc.fit(dataframeData.values, dataframeUser.values.ravel())
+le8 = LabelEncoder()
+dataframeMovie["runtime"] = le8.fit_transform(dataframeMovie["runtime"])
 
-# # Prédiction sur les données d'entraînement
-# predicted_values = rfc.predict(dataframeData.values)
-
-# # Affichage des valeurs prédites
-# print("Valeurs prédites :")
-# print(predicted_values)
+print("----------Fin Encodage-------------")
+print("-----------------------------------")
+print("-----Entrainement du modèle--------")
+rfc = RandomForestClassifier(
+    n_estimators=10,
+    max_depth=3,
+    random_state=0,
+)
+rfc = rfc.fit(dataframeUserLikeData, dataframeUserLike)  
+print("-----------------------------------")
+# Prédiction sur les données d'entraînement
+print("--------Model prevision------------")
+predicted_values = rfc.predict(dataframeMovie)
+print("-----------------------------------")
+print("Valeurs prédites :", predicted_values)
+print("-----------------------------------")
+## Si on veux tester la précision du modèle
+## Il faut alors les mêmes données en entrainement et en test
+#print("Précision :", accuracy_score(dataframeMovie, predicted_values))
+print("-----------------------------------")
 
