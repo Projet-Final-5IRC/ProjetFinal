c4df4a0f859c5d85aaa86a461753f01c4dfffab0
Maxime Brossard <maxime.brossard5@gmail.com>
2025-01-10 16:39:39 +0100

    feat: adding preference to crud

---
+++ data/data/Models/EntityFramework/Preference.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace data.Models.EntityFramework
+{
+    [Table("t_e_preference_utl")]
+    [Index(nameof(IdPreference))]
+    [PrimaryKey(nameof(IdPreference))]
+    public class Preference
+    {
+        [Key]
+        [Column("prf_id")]
+        public int IdPreference { get; set; }
+
+        [Required]
+        [Column("usr_id")]
+        public int IdUser { get; set; }
+
+        [Required]
+        [Column("gen_id")]
+        public int IdGenre { get; set; }
+
+        [ForeignKey(nameof(IdUser))]
+        [InverseProperty(nameof(Users.UserPreference))]
+        public virtual Users? UserReference { get; set; } = null!;
+
+        [ForeignKey(nameof(IdGenre))]
+        [InverseProperty(nameof(Genres.GenrePreference))]
+        public virtual Genres? GenreReference { get; set; } = null!;
+
+        public void UpdatePreferenceValue(Preference preference)
+        {
+            this.IdPreference = preference.IdPreference;
+            this.IdUser = preference.IdUser;
+            this.IdGenre = preference.IdGenre;
+        }
+    }
+}
