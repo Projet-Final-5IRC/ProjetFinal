b96fccd8acba517faa6823446f3694e6309fcc63
Maxime Brossard <maxime.brossard5@gmail.com>
2025-01-11 12:58:43 +0100

    fix: bad test

--- data/dataTests/Controllers/EventInviteControllerTests.cs
+++ data/dataTests/Controllers/EventInviteControllerTests.cs
@@ -52,8 +52,8 @@ namespace data.Controllers.Tests
         [TestMethod]
         public async Task GetInviteByID_SuccessGetInviteByID()
         {
-            var result = await _controller.GetInviteById(2);
-            var invitInDB = _context.EventInvite.Where(c => c.idEventsInvite == 2).FirstOrDefault();
+            var result = await _controller.GetInviteById(3);
+            var invitInDB = _context.EventInvite.Where(c => c.idEventsInvite == 3).FirstOrDefault();
 
             Assert.IsNotNull(result);
             Assert.AreEqual(invitInDB.idEventsInvite, result.Value.IdEventsInvite);
