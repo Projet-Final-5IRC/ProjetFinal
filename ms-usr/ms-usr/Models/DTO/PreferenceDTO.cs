2df36db633171756907f5e967982e8905cb2b185
Maxime Brossard <maxime.brossard5@gmail.com>
2025-01-11 11:52:05 +0100

    feat: init user adding preference

---
+++ ms-usr/ms-usr/Models/DTO/PreferenceDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace ms_usr.Models.DTO
+{
+    public class PreferenceDTO
+    {
+        public int IdPreference { get; set; }
+        public int IdUser { get; set; }
+        public int IdGenre { get; set; }
+    }
+}
