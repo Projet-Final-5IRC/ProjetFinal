1be858abdde43a374792eca59cf738f34ed13a0b
Maxime Brossard <maxime.brossard5@gmail.com>
2025-01-11 11:52:05 +0100

    feat: init user adding preference

---
+++ ms-usr/ms-usr/Services/DataService.cs
@@ -0,0 +1,24 @@
+﻿using ms_usr.Models.DTO;
+using System.Net;
+
+namespace ms_usr.Services
+{
+    public class DataService
+    {
+        private readonly HttpClient _httpClient;
+
+        public DataService(HttpClient httpClient, IConfiguration configuration)
+        {
+            _httpClient = httpClient;
+
+            var apiBaseUrl = configuration.GetValue<string>("ConnectionStrings:BaseURL");
+
+            _httpClient.BaseAddress = new Uri(apiBaseUrl);
+        }
+
+        public async Task<List<PreferenceDTO>> GetAllPreferences(string endpoint, string idUser)
+        {
+
+        }
+    }
+}
