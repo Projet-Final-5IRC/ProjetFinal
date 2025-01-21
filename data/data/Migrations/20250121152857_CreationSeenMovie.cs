6647bf21d23d0b9698f766cc901d7c4c2540ed8e
Maxime Brossard <maxime.brossard5@gmail.com>
2025-01-21 17:43:08 +0100

    feat: adding the possibility for user to see and like movie

---
+++ data/data/Migrations/20250121152857_CreationSeenMovie.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace data.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreationSeenMovie : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "t_e_liked_movies_utl",
+                schema: "public",
+                columns: table => new
+                {
+                    lik_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    lik_idtmdb = table.Column<int>(type: "integer", nullable: false),
+                    usr_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_t_e_liked_movies_utl", x => x.lik_id);
+                    table.ForeignKey(
+                        name: "fk_liked_movie",
+                        column: x => x.usr_id,
+                        principalSchema: "public",
+                        principalTable: "t_e_users_utl",
+                        principalColumn: "usr_id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "t_e_seen_movies_utl",
+                schema: "public",
+                columns: table => new
+                {
+                    sem_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    sem_idtmdb = table.Column<int>(type: "integer", nullable: false),
+                    usr_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_t_e_seen_movies_utl", x => x.sem_id);
+                    table.ForeignKey(
+                        name: "fk_seen_movie",
+                        column: x => x.usr_id,
+                        principalSchema: "public",
+                        principalTable: "t_e_users_utl",
+                        principalColumn: "usr_id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_t_e_liked_movies_utl_lik_id",
+                schema: "public",
+                table: "t_e_liked_movies_utl",
+                column: "lik_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_t_e_liked_movies_utl_usr_id",
+                schema: "public",
+                table: "t_e_liked_movies_utl",
+                column: "usr_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_t_e_seen_movies_utl_sem_id",
+                schema: "public",
+                table: "t_e_seen_movies_utl",
+                column: "sem_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_t_e_seen_movies_utl_usr_id",
+                schema: "public",
+                table: "t_e_seen_movies_utl",
+                column: "usr_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "t_e_liked_movies_utl",
+                schema: "public");
+
+            migrationBuilder.DropTable(
+                name: "t_e_seen_movies_utl",
+                schema: "public");
+        }
+    }
+}
---
+++ data/data/Models/DataManager/LikedMoviesManager.cs
@@ -0,0 +1,179 @@
+﻿using data.Models.DTO;
+using data.Models.EntityFramework;
+using data.Models.Repository;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace data.Models.DataManager
+{
+    public class LikedMoviesManager : IDataRepositoryWithMovies<LikedMovies>
+    {
+
+        readonly EventDBContext? eventDBContext;
+
+        public LikedMoviesManager(EventDBContext eventDBContext)
+        {
+            this.eventDBContext = eventDBContext;
+        }
+
+
+        public async Task<ActionResult<LikedMovies>> AddAsync(LikedMovies entity)
+        {
+            if (eventDBContext == null)
+            {
+                throw new NullReferenceException(nameof(eventDBContext));
+            }
+            if (entity == null)
+            {
+                return new BadRequestResult();
+            }
+
+            try
+            {
+                await eventDBContext.LikedMovies.AddAsync(entity);
+                await eventDBContext.SaveChangesAsync();
+                return entity;
+            }
+            catch (Exception ex)
+            {
+                return new ObjectResult($"An error occurred while adding the preference: {ex.Message}")
+                {
+                    StatusCode = StatusCodes.Status500InternalServerError
+                };
+            }
+        }
+
+        public async Task<ActionResult> DeleteAsync(LikedMovies entity)
+        {
+            if (eventDBContext == null)
+            {
+                throw new NullReferenceException(nameof(eventDBContext));
+            }
+            if (entity == null)
+            {
+                return new BadRequestResult();
+            }
+
+            try
+            {
+                eventDBContext.LikedMovies.Remove(entity);
+                await eventDBContext.SaveChangesAsync();
+                return new NoContentResult();
+            }
+            catch (Exception ex)
+            {
+                return new ObjectResult($"An error occurred while deleting the preference: {ex.Message}")
+                {
+                    StatusCode = StatusCodes.Status500InternalServerError
+                };
+            }
+        }
+
+        public async Task<ActionResult> DeleteMoviesByUserAndMovieId(int IdUser, int movieTmdbId)
+        {
+            if (eventDBContext == null)
+            {
+                throw new NullReferenceException(nameof(eventDBContext));
+            }
+            if (IdUser == null && movieTmdbId == null)
+            {
+                return new BadRequestResult();
+            }
+
+            try
+            {
+                var entity = await eventDBContext.LikedMovies.FirstOrDefaultAsync(e => e.IdUser == IdUser && e.IdTmdbMovie == movieTmdbId);
+                eventDBContext.LikedMovies.Remove(entity);
+                await eventDBContext.SaveChangesAsync();
+                return new NoContentResult();
+            }
+            catch (Exception ex)
+            {
+                return new ObjectResult($"An error occurred while deleting the preference: {ex.Message}")
+                {
+                    StatusCode = StatusCodes.Status500InternalServerError
+                };
+            }
+        }
+
+        public async Task<ActionResult<IEnumerable<LikedMovies>>> GetAllAsync()
+        {
+            if (eventDBContext == null)
+            {
+                throw new NullReferenceException(nameof(eventDBContext));
+            }
+            try
+            {
+                return await eventDBContext.LikedMovies.ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                return new ObjectResult($"An error occurred while deleting the preference: {ex.Message}")
+                {
+                    StatusCode = StatusCodes.Status500InternalServerError
+                };
+            }
+        }
+
+        public async Task<ActionResult<LikedMovies>> GetByIdAsync(int id)
+        {
+            if (eventDBContext == null)
+            {
+                throw new NullReferenceException(nameof(eventDBContext));
+            }
+
+            try
+            {
+                var result = await eventDBContext.LikedMovies.FirstOrDefaultAsync(e => e.IdLikedMovies == id);
+                return result != null ? result : new NotFoundResult();
+            }
+            catch (Exception ex)
+            {
+                return new ObjectResult($"An error occurred while deleting the preference: {ex.Message}")
+                {
+                    StatusCode = StatusCodes.Status500InternalServerError
+                };
+            }
+        }
+
+        public async Task<List<LikedMovies>> GetMoviesByUserId(int IdUser)
+        {
+            if (eventDBContext == null)
+            {
+                throw new NullReferenceException(nameof(eventDBContext));
+            }
+
+            return await eventDBContext.LikedMovies.Where(e => e.IdUser == IdUser).ToListAsync();
+        }
+
+        public async Task<ActionResult<LikedMovies>> UpdateAsync(LikedMovies entityToUpdate, LikedMovies entityUpdated)
+        {
+            if (eventDBContext == null)
+            {
+                throw new NullReferenceException(nameof(eventDBContext));
+            }
+
+            if (entityUpdated == null && entityToUpdate == null)
+            {
+                return new BadRequestResult();
+            }
+
+            eventDBContext.Entry(entityToUpdate).State = EntityState.Modified;
+            entityToUpdate.UpdateLikeMovieValue(entityUpdated);
+
+            try
+            {
+                await eventDBContext.SaveChangesAsync();
+                return entityToUpdate;
+            }
+            catch (Exception ex)
+            {
+                return new ObjectResult($"An error occurred while updating the preference: {ex.Message}")
+                {
+                    StatusCode = StatusCodes.Status500InternalServerError
+                };
+            }
+        }
+    }
+}
