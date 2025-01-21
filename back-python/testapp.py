e2a9942cabcf6621b7342161d422f2c6b944756c
GJBoX <julien.buc2@gmail.com>
2025-01-21 11:55:19 +0100

    Ok, manque connexion bdd avec recoUser

--- back-python/testapp.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask, request, jsonify
-import config  # Assurez-vous que config est importé correctement
-from app import main
-
-resultat = main.MainFunction()  # Assurez-vous que MainFunction prend les bons paramètres si nécessaire
-print(resultat)
