7cc9a93cf5c5264c7a4ef22932c1eba038779dfb
Brandon Hunt <116546225+Ashersxm@users.noreply.github.com>
2025-01-22 09:11:24 -0400

    TU

---
+++ ms-recommend-net/Interfaces/ITmdbService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace ms_recommend_net.Services
+{
+    public interface ITmdbService
+    {
+        Task<int?> GetMovieIdAsync(string movieTitle);
+        Task<string> GetMovieDetailsAsync(int movieId);
+    }
+}
